3e4307efd8251b400c50cb719009763152330bd6
unknown <revatiaxworkz@gmail.com>
2024-07-30 22:34:29 -0700

    adding 30 july task

---
+++ revati/Fish.java
@@ -0,0 +1,16 @@
+//Parameterized Constructor
+class Fish
+{
+	String name;
+	String type;
+	
+	Fish(String nameLocal , String typeLocal)
+	{
+		System.out.println("Created board with name, type");
+		System.out.println("name :"+name);
+		System.out.println("type :"+type);
+		System.out.println("Name Local :"+nameLocal);
+		System.out.println("type Local :"+typeLocal);
+		
+	}
+}
---
+++ revati/FishRunner.java
@@ -0,0 +1,8 @@
+class FishRunner{
+public static void main(String[] args)
+{
+System.out.println("running main in FishRunner");
+Fish fish=new Fish("tiny",67,"black");
+
+}
+}
